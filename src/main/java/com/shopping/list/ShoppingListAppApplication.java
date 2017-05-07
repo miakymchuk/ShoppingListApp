a8986bbb0c024f23f2a4573513e25bdbe9d35b87
m.iakymchuk <maksym.iakymchuk@gmail.com>
2017-05-07 21:43:01 +0300

    generate skeleton app from spring boot initializer

---
+++ src/main/java/com/shopping/list/ShoppingListAppApplication.java
@@ -0,0 +1,12 @@
+package com.shopping.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListAppApplication.class, args);
+	}
+}
